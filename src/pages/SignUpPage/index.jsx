16bdc30febe4dad7a58e3eb783897fadd17f52e8
lottie-isobel <charlotteeaston@hortmail.co.uk>
2024-03-05 15:07:38 +0000

    create form for login page

--- src/pages/SignUpPage/index.jsx
+++ src/pages/SignUpPage/index.jsx
@@ -6,31 +6,29 @@ export default function SignUpPage() {
     <div className="signup-page">
       <h1>Create an account</h1>
       <form className="signup">
-        <div className="name">
-          <input
-            type="text"
-            id="first_name"
-            name="first_name"
-            placeholder="first name"
-            required
-          />
-          <input
-            type="text"
-            id="last_name"
-            name="last_name"
-            placeholder="last name"
-            required
-          />
-        </div>
-        <div className="email">
-          <input
-            type="text"
-            id="email"
-            name="email"
-            placeholder="email address"
-            required
-          />
-        </div>
+        <input
+          type="text"
+          id="first_name"
+          name="first_name"
+          placeholder="first name"
+          required
+        />
+        <input
+          type="text"
+          id="last_name"
+          name="last_name"
+          placeholder="last name"
+          required
+        />
+
+        <input
+          type="text"
+          id="email"
+          name="email"
+          placeholder="email address"
+          required
+        />
+
         <div className="userdetails">
           <input
             type="text"
@@ -39,15 +37,10 @@ export default function SignUpPage() {
             placeholder="password"
             required
           />
-          <input
-            type="text"
-            id="username"
-            name="username"
-            placeholder="username"
-            required
-          />
         </div>
-        <input className="signup-button" type="submit" value="Sign Up"></input>
+        <div class="button-wrap">
+          <input className="signup-button" type="submit" value="Sign-up" />
+        </div>
       </form>
     </div>
   );
