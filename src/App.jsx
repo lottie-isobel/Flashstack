733db4b09365397b99d00456bc97f6ffe17154e7
Rahma Ali <rahma.ma078@gmail.com>
2024-03-05 14:06:13 +0000

    add use context wrap

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,29 @@ import './App.css'
 import { Routes, Route, Outlet } from 'react-router-dom';
 import * as Pages from './pages'
 import { Header, Footer } from './components'
+import { EditorProvider } from './contexts';
 
 function App() {
 
   return (
     <>
-      <Header />
-      <div className='content'>
-      <Routes>
-        <Route path="/signup" element={<Pages.SignUpPage />} />
-        <Route path="/login" element={<Pages.LoginPage />} />
-        <Route path="/" element={<Pages.Dashboard />} />
-        <Route path="/flashcards" element={<Pages.AllFlashcardsPage />} />
-        <Route path="/notes" element={<Pages.AllNotesPage />} />
-        <Route path="/newflashcard" element={<Pages.CreateFlashcardPage />} />
-        <Route path="/newnote" element={<Pages.CreateNotePage />} />
-        <Route path="/revise" element={<Pages.RevisionPage />} />
-        <Route path="*" element={<Pages.NotFoundPage />} />
-      </Routes>
-      </div>
-      <Footer />
+      <EditorProvider>
+        <Header />
+        <div className='content'>
+        <Routes>
+          <Route path="/signup" element={<Pages.SignUpPage />} />
+          <Route path="/login" element={<Pages.LoginPage />} />
+          <Route path="/" element={<Pages.Dashboard />} />
+          <Route path="/flashcards" element={<Pages.AllFlashcardsPage />} />
+          <Route path="/notes" element={<Pages.AllNotesPage />} />
+          <Route path="/newflashcard" element={<Pages.CreateFlashcardPage />} />
+          <Route path="/newnote" element={<Pages.CreateNotePage />} />
+          <Route path="/revise" element={<Pages.RevisionPage />} />
+          <Route path="*" element={<Pages.NotFoundPage />} />
+        </Routes>
+        </div>
+        <Footer />
+      </EditorProvider>
     </>
   )
 }
