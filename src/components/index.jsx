53190dec75830a6088ae7166fa80eedbbd56582e
Rahma Ali <rahma.ma078@gmail.com>
2024-03-05 14:08:56 +0000

    add components for delete, save and add equation buttons

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,3 +2,4 @@ export { default as Header } from "./Header";
 export { default as Footer } from "./Footer";
 export { default as Card } from "./Card"
 export { default as StylingControls } from "./StylingControls"
+export { default as CardControls } from "./CardControls"
