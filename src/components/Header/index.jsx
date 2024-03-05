9db0e34ce79e15125a5d94cdea463a5c7e722b6b
lottie-isobel <charlotteeaston@hortmail.co.uk>
2024-03-05 09:11:31 +0000

    Initiate styling for header/footer

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import logo from "../../assets/fslogo.png";
 
 export default function Header() {
   return (
     <>
-        <header>
-            <nav>
-                <NavLink to="/flashcards">Flashcards</NavLink>
-                <NavLink to="/notes">Shows</NavLink>
-                <NavLink to="/logout">Sign Out</NavLink>
-            </nav>
-        </header>
+      <header className="nav-bar">
+        <img src={logo} className="logo" />
+        <nav>
+          <button className="btn-nav">
+            <NavLink to="/flashcards">Flashcards</NavLink>
+          </button>
+          <button className="btn-nav">
+            <NavLink to="/notes">Shows</NavLink>
+          </button>
+          <button className="btn-nav">
+            <NavLink to="/logout">Sign Out</NavLink>
+          </button>
+        </nav>
+      </header>
     </>
-  )
+  );
 }
