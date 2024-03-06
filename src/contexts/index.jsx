8b496e0e9689b446a28d780f6be367d7d6587c36
Tom Wood <dexterbyte01@gmail.com>
2024-03-06 10:11:34 +0000

    add auth context

--- src/contexts/index.jsx
+++ src/contexts/index.jsx
@@ -1,6 +1,17 @@
-import React, { useRef, useContext, createContext } from "react";
-//Good template for createContext
+import React, { useRef, useState, useContext, createContext } from "react";
+
 const EditorContext = createContext();
+const AuthContext = createContext()
+
+export const AuthProvider = ({ children }) => {
+  const authenticated = useState(false)
+  return (
+    <AuthContext.Provider value={{authenticated}}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
 export const EditorProvider = ({ children }) => {
     const titleRef = useRef(null);
     const textRef = useRef(null);  
@@ -11,4 +22,5 @@ export const EditorProvider = ({ children }) => {
   );
 };
 
+export const useAuth = () => useContext(AuthContext)
 export const useEditor = () => useContext(EditorContext);
