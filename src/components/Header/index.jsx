7b8d33e5dd0c96b88de986f65566a702f978ec02
Tom Wood <dexterbyte01@gmail.com>
2024-03-06 11:48:44 +0000

    dynamically change header buttons

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { useAuth } from "../../hooks/useAuth"; 
 import logo from "../../assets/flashstack-logo.png";
 import "./index.css";
 
 export default function Header() {
+  const { token } = useAuth()
   return (
     <>
       <header className="nav-bar">
@@ -15,21 +17,26 @@ export default function Header() {
                 className="d-inline-block align-top logo"
               />
         <nav>
-          <button className="custom-button">
+          {token && <button className="custom-button">
             <NavLink to="/flashcards" className="nav-link">
               Flashcards
             </NavLink>
-          </button>
-          <button className="custom-button">
+          </button>}
+          {token && <button className="custom-button">
             <NavLink to="/notes" className="nav-link">
               Notes
             </NavLink>
-          </button>
-          <button className="custom-button">
+          </button>}
+          {token && <button className="custom-button">
             <NavLink to="/logout" className="nav-link">
               Sign Out
             </NavLink>
-          </button>
+          </button>}
+          {!token && <button className="custom-button">
+            <NavLink to="/login" className="nav-link">
+              Sign In
+            </NavLink>
+          </button>}
         </nav>
       </header>
       <div className="line-break"></div>
