231c51a6350320da39746ddd68b79ee98f05d6ff
Rahma Ali <rahma.ma078@gmail.com>
2024-03-07 14:14:56 +0000

    update the fetch requests

--- src/components/UpdateCardBtn/index.jsx
+++ src/components/UpdateCardBtn/index.jsx
@@ -1,35 +1,14 @@
 import React from 'react';
+import { AuthProvider, useAuth } from '../../hooks/useAuth';
 
 export default function UpdateCardBtn() {
 
+    const { userid } = useAuth();
+
     const handleUpdate = async () => {
 
-        const tokenStr = localStorage.getItem('token')
 
-        console.log(tokenStr)
-
-        const token = tokenStr.replace(/^"(.*)"$/, '$1')
-
-        console.log(token)
-
-        const options = {
-            method: "GET",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-        }
-
-        const response = await fetch(`https://flashstack-backend.onrender.com/user/${token}`, options);
-        const data = await response.json();
-
-        if (response.status == 200) {
-            alert(`The user id is ${data.id}.`)
-        } else {
-            alert(data.error);
-        }
-
-        const oldCategory = 'test'
+       const oldCategory = 'test'
 
         console.log(oldCategory)
 
@@ -39,44 +18,48 @@ export default function UpdateCardBtn() {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
+            body: JSON.stringify({
+                category: oldCategory,
+                userid: userid
+            })
         }
 
-        const cateResponse = await fetch(`https://flashstack-backend.onrender.com/note/${oldCategory}/${data.id}`, cateOptions);
+        const cateResponse = await fetch(`https://flashstack-backend.onrender.com/note/category/`, cateOptions);
         const cateData = await cateResponse.json();
 
         if (cateResponse.status == 200) {
-            alert(`The note id is ${cateData.id}.`)
+            alert(cateData)
         } else {
             alert(cateData.error);
         }
 
 
-    const updatedNote = {
-        content: localStorage.getItem('content'),
-        category: 'testing'
-    };
+    // const updatedNote = {
+    //     content: localStorage.getItem('content'),
+    //     category: 'testing'
+    // };
 
-    console.log(updatedNote)
+    // console.log(updatedNote)
 
-    const noteOptions = {
-        method: "PATCH",
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(updatedNote)
-    }
+    // const noteOptions = {
+    //     method: "PATCH",
+    //     headers: {
+    //         'Accept': 'application/json',
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify(updatedNote)
+    // }
 
-    const noteResponse = await fetch(`https://flashstack-backend.onrender.com/note/${cateData.id}`, noteOptions);
-    const noteData = await noteResponse.json();
+    // const noteResponse = await fetch(`https://flashstack-backend.onrender.com/note/${cateData.id}`, noteOptions);
+    // const noteData = await noteResponse.json();
 
-    if (noteResponse.status == 200) {
-        e.target.reset();
-        alert(`You're note content has been updated to ${noteData.content} and you're note category has been updated to${noteData.category}!`)
-        console.log('Data updated successfully:', noteData);
-    } else {
-        alert(noteData.error);
-    }
+    // if (noteResponse.status == 200) {
+    //     e.target.reset();
+    //     alert(`You're note content has been updated to ${noteData.content} and you're note category has been updated to${noteData.category}!`)
+    //     console.log('Data updated successfully:', noteData);
+    // } else {
+    //     alert(noteData.error);
+    // }
     };
 
   return (
