5bc3e6053af053d4b909f496ea445facac2bf51f
Tom Wood <dexterbyte01@gmail.com>
2024-03-08 09:48:01 +0000

    fix revise page

--- src/pages/RevisionPage/index.jsx
+++ src/pages/RevisionPage/index.jsx
@@ -19,6 +19,7 @@ export default function RevisionPage() {
       const response = await fetch(
         `https://flashstack-backend.onrender.com/card/deck/${id}`
       );
+      console.log(response)
       const data = await response.json();
       const shuffledCards = data
         .map((value) => ({ value, sort: Math.random() }))
@@ -37,13 +38,13 @@ export default function RevisionPage() {
     setCurrentCard(cards[(currentCardIndex + 1) % cards.length]);
   };
 
-  console.log(currentCard)
   return (
     <div>
       <h1>Revise</h1>
       <h2 className="cards-revised">Cards revised: {cardsRevised}</h2>
       {currentCard ? (
         <RevisionCard
+          key={currentCardIndex} // Ensure re-render when currentCard changes
           question={currentCard.question}
           answer={currentCard.answer}
         />
